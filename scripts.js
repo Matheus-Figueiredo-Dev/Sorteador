2a966b4640bb17527106f25cf3ad25c10a03ef73
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-04-16 09:59:58 -0300

    Atualização no background

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 function generateNumber() {
-    const min = Math.ceil(document.querySelector(".input-min").value)
-    const max = Math.floor(document.querySelector(".input-max").value)
+    const min = Math.ceil(parseInt(document.querySelector(".input-min").value))
+    const max = Math.floor(parseInt(document.querySelector(".input-max").value))
     const quantity = parseInt(document.querySelector(".input-quantity").value)
 
+    // Verifica se os valores de min e max são válidos
     if (min >= max) {
         alert("O valor mínimo tem que ser menor que o valor máximo")
+        return;  // Interrompe a execução da função caso a condição seja atendida
     }
 
-    else {
-        let results = []
-        for (let i = 0; i < quantity; i++) {
-            const result = Math.floor(Math.random() * (max - min + 1)) + min;
-            results.push(result)
-        }
-        const resultDisplay = document.querySelector(".result-display")
-        resultDisplay.textContent = `Os números sorteados são ${results.join(", ")}`
+    let results = []
+    for (let i = 0; i < quantity; i++) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        results.push(result)
     }
 
+    const resultDisplay = document.querySelector(".result-display")
+    resultDisplay.textContent = `Os números sorteados são ${results.join(", ")}`
 }
